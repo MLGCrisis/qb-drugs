7078137d6f8f84a65939664affc9cd7dfca20df4
GG79 <midlifegamingcrisis@gmail.com>
2023-03-17 08:27:58 +0200

    changed drugs

--- config.lua
+++ config.lua
@@ -1,14 +1,14 @@
 Config = Config or {}
 Config.Dealers = {}
 Config.UseTarget = GetConvar('UseTarget', 'false') == 'true'
-Config.PoliceCallChance = 15
+Config.PoliceCallChance = 10
 
 -- Shop Config
 Config.Products = {
     [1] = {
         name = "weed_white-widow",
         price = 15,
-        amount = 150,
+        amount = 30,
         info = {},
         type = "item",
         slot = 1,
@@ -16,134 +16,161 @@ Config.Products = {
     },
     [2] = {
         name = "weed_skunk",
-        price = 15,
-        amount = 150,
+        price = 40,
+        amount = 30,
         info = {},
         type = "item",
         slot = 2,
-        minrep = 20,
+        minrep = 5,
     },
     [3] = {
         name = "weed_purple-haze",
-        price = 15,
-        amount = 150,
+        price = 70,
+        amount = 30,
         info = {},
         type = "item",
         slot = 3,
-        minrep = 40,
+        minrep = 10,
     },
     [4] = {
         name = "weed_og-kush",
-        price = 15,
-        amount = 150,
+        price = 150,
+        amount = 30,
         info = {},
         type = "item",
         slot = 4,
-        minrep = 60,
+        minrep = 15,
     },
     [5] = {
         name = "weed_amnesia",
-        price = 15,
-        amount = 150,
+        price = 420,
+        amount = 30,
         info = {},
         type = "item",
         slot = 5,
-        minrep = 80,
+        minrep = 20,
     },
     [6] = {
         name = "weed_white-widow_seed",
-        price = 15,
-        amount = 150,
+        price = 150,
+        amount = 25,
         info = {},
         type = "item",
         slot = 6,
-        minrep = 100,
+        minrep = 25,
     },
     [7] = {
         name = "weed_skunk_seed",
-        price = 15,
-        amount = 150,
+        price = 700,
+        amount = 25,
         info = {},
         type = "item",
         slot = 7,
-        minrep = 120,
+        minrep = 30,
     },
     [8] = {
         name = "weed_purple-haze_seed",
-        price = 15,
-        amount = 150,
+        price = 1100,
+        amount = 25,
         info = {},
         type = "item",
         slot = 8,
-        minrep = 140,
+        minrep = 35,
     },
     [9] = {
         name = "weed_og-kush_seed",
-        price = 15,
-        amount = 150,
+        price = 2200,
+        amount = 25,
         info = {},
         type = "item",
         slot = 9,
-        minrep = 160,
+        minrep = 40,
     },
     [10] = {
         name = "weed_amnesia_seed",
-        price = 15,
-        amount = 150,
+        price = 7500,
+        amount = 25,
         info = {},
         type = "item",
         slot = 10,
-        minrep = 180,
+        minrep = 45,
+    },
+    [11] = {
+        name = "crack_baggy",
+        price = 600,
+        amount = 15,
+        info = {},
+        type = "item",
+        slot = 10,
+        minrep = 60,
+    },
+    [12] = {
+        name = "cokebaggy",
+        price = 700,
+        amount = 15,
+        info = {},
+        type = "item",
+        slot = 10,
+        minrep = 75,
+    },
+    [13] = {
+        name = "meth",
+        price = 800,
+        amount = 15,
+        info = {},
+        type = "item",
+        slot = 10,
+        minrep = 90,
     },
 }
 
 -- Selling Config
-Config.SuccessChance = 50
-Config.ScamChance = 25
-Config.RobberyChance = 25
+Config.SuccessChance = 75
+Config.ScamChance = 10
+Config.RobberyChance = 15
 Config.MinimumDrugSalePolice = 0
 
 Config.DrugsPrice = {
     ["weed_white-widow"] = {
-        min = 15,
-        max = 24,
+        min = 20,
+        max = 30,
     },
     ["weed_og-kush"] = {
-        min = 15,
-        max = 28,
+        min = 180,
+        max = 215,
     },
     ["weed_skunk"] = {
-        min = 15,
-        max = 31,
+        min = 50,
+        max = 80,
     },
     ["weed_amnesia"] = {
-        min = 18,
-        max = 34,
+        min = 500,
+        max = 650,
     },
     ["weed_purple-haze"] = {
-        min = 18,
-        max = 37,
+        min = 80,
+        max = 110,
     },
     ["weed_ak47"] = {
-        min = 18,
-        max = 40,
+        min = 700,
+        max = 750,
     },
     ["crack_baggy"] = {
-        min = 18,
-        max = 34,
+        min = 800,
+        max = 850,
     },
     ["cokebaggy"] = {
-        min = 18,
-        max = 37,
+        min = 850,
+        max = 900,
     },
     ["meth"] = {
-        min = 18,
-        max = 40,
+        min = 900,
+        max = 1050,
     },
 }
 
 -- Delivery Config
-Config.UseMarkedBills = false -- true for marked bills, false for cash
+Config.UseMarkedBills = true -- true for marked bills, false for cash
 Config.DeliveryRepGain = 1 -- amount of rep gained per delivery
 Config.DeliveryRepLoss = 1 -- amount of rep lost if delivery wrong or late
 Config.PoliceDeliveryModifier = 2 -- amount to multiply active cop count by
@@ -154,12 +181,12 @@ Config.DeliveryItems = {
     [1] = {
         ["item"] = "weed_brick",
         ["minrep"] = 0,
-        ['payout'] = 1000
+        ['payout'] = 1750
     },
     [2] = {
         ["item"] = "coke_brick",
         ["minrep"] = 0,
-        ['payout'] = 1000
+        ['payout'] = 1750
     },
 }
 
